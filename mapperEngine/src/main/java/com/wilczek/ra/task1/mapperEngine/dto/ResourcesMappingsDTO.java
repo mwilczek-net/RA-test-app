7d0e401001051c94d5cc235260e8ec5e58eee8a9
mwilczek-net <mwilczek-net@users.noreply.github.com>
2020-10-27 17:13:39 +0100

    ResourcesMappingsDTOTest.getMappings_test()

--- mapperEngine/src/main/java/com/wilczek/ra/task1/mapperEngine/dto/ResourcesMappingsDTO.java
+++ mapperEngine/src/main/java/com/wilczek/ra/task1/mapperEngine/dto/ResourcesMappingsDTO.java
@@ -2,12 +2,16 @@ package com.wilczek.ra.task1.mapperEngine.dto;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 @Service("resourcesMappingsDTO")
 @Primary
 public class ResourcesMappingsDTO implements MappingsDTO {
+
+	@Value("${resources.mappings.file}")
+	private String mappingsFileUrl;
 
 	@Override
 	public List<String> getMappings() {
---
+++ mapperEngine/src/test/java/com/wilczek/ra/task1/mapperEngine/dto/ResourcesMappingsDTOTest.java
@@ -0,0 +1,28 @@
+package com.wilczek.ra.task1.mapperEngine.dto;
+
+import static org.junit.jupiter.api.Assertions.assertLinesMatch;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = { ResourcesMappingsDTO.class })
+class ResourcesMappingsDTOTest {
+
+	public static final List<String> MAPPINGS_LIST = Arrays.asList("Animals", "Furniture");
+
+	@Autowired
+	@Qualifier("resourcesMappingsDTO")
+	private ResourcesMappingsDTO mappingsDTO;
+
+	@Test
+	void getMappings_test() {
+		final List<String> mappings = mappingsDTO.getMappings();
+		assertLinesMatch(MAPPINGS_LIST, mappings);
+	}
+
+}
