0e2413bcac4c25b3e478ed5f1d72c7cfd9dcf092
mwilczek-net <mwilczek-net@users.noreply.github.com>
2020-10-28 17:33:09 +0100

    MappingsValuesController.mappingsList()

--- mapperRestApi/src/main/java/com/wilczek/ra/task1/mapperRestApi/mappings/MappingsValuesController.java
+++ mapperRestApi/src/main/java/com/wilczek/ra/task1/mapperRestApi/mappings/MappingsValuesController.java
@@ -1,19 +1,32 @@
 package com.wilczek.ra.task1.mapperRestApi.mappings;
 
 import java.io.IOException;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.wilczek.ra.task1.mapperEngine.service.MappingService;
 import com.wilczek.ra.task1.mapperEngine.utils.ResourceHelper;
 
 @RestController
 @RequestMapping("/api")
 public class MappingsValuesController {
 
+	@Autowired
+	@Qualifier("defaultMappingService")
+	private MappingService mappingService;
+
 	@RequestMapping("/hello")
 	public String helloWorld() throws IOException {
 		return ResourceHelper.readResourceToString("html/welcome.html");
+	}
+
+	@RequestMapping("/mappingsList")
+	public List<String> mappingsList() throws Exception {
+		return mappingService.getMappings();
 	}
 
 
