36f022b3750d8ffe6893f087b12cab5bcc37a5cf
mwilczek-net <mwilczek-net@users.noreply.github.com>
2020-10-28 17:52:22 +0100

    Added size path, list path with slashes

--- mapperRestApi/src/main/java/com/wilczek/ra/task1/mapperRestApi/mappings/MappingsValuesController.java
+++ mapperRestApi/src/main/java/com/wilczek/ra/task1/mapperRestApi/mappings/MappingsValuesController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,10 +25,13 @@ public class MappingsValuesController {
 		return ResourceHelper.readResourceToString("html/welcome.html");
 	}
 
-	@RequestMapping("/mappingsList")
+	@RequestMapping("/mappings/list")
 	public List<String> mappingsList() throws Exception {
 		return mappingService.getMappings();
 	}
 
-
+	@RequestMapping("/mappings/size/name/{mappingName}")
+	public int mappingsSize(@PathVariable("mappingName") final String mappingName) throws Exception {
+		return mappingService.getMappingSize(mappingName);
+	}
 }
