ed3c9af7fd7e4c9c796c5229ae54939d79975179
mwilczek-net <mwilczek-net@users.noreply.github.com>
2020-10-28 18:59:46 +0100

    First implementation for map endpoint
    Needs refactor and wrapping into more complex format

--- mapperRestApi/src/main/java/com/wilczek/ra/task1/mapperRestApi/controllers/MappingsController.java
+++ mapperRestApi/src/main/java/com/wilczek/ra/task1/mapperRestApi/controllers/MappingsController.java
@@ -1,11 +1,14 @@
 package com.wilczek.ra.task1.mapperRestApi.controllers;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.wilczek.ra.task1.mapperEngine.service.MappingService;
@@ -13,6 +16,8 @@ import com.wilczek.ra.task1.mapperEngine.service.MappingService;
 @RestController
 @RequestMapping("/api/mappings")
 public class MappingsController {
+
+	public static final String DEFAULT_SEPARATOR = ",";
 
 	@Autowired
 	@Qualifier("defaultMappingService")
@@ -31,5 +36,21 @@ public class MappingsController {
 	@RequestMapping("/size/id/{mappingId}")
 	public int mappingsSize(@PathVariable("mappingId") final int mappingId) throws Exception {
 		return mappingService.getMappingSize(mappingId);
+	}
+
+	@RequestMapping("/map/name/{mappingName}")
+	public List<String> map(@PathVariable("mappingName") final String mappingName,
+			@RequestParam("numbers") final String numbers,
+			@RequestParam(name = "separator", required = false) final String separator
+			) throws Exception {
+
+		final List<Integer> numbersList = Arrays.asList(numbers.split(","))
+				.stream()
+				.map(number -> Integer.valueOf(number))
+				.collect(Collectors.toList());
+
+		final String selectedSeparator = separator != null ? separator : DEFAULT_SEPARATOR;
+
+		return mappingService.mapAndJoinPrevious(mappingName, numbersList, selectedSeparator);
 	}
 }
